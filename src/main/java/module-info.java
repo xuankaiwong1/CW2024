4faf4d750422c72072cdc0d5bc579036e14ce7d7
xuankaiwong1 <wongxuankai123@gmail.com>
2024-12-13 00:49:46 +0800

    corrected the packages and classes path

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,19 @@
 module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.media;
 
-
-    opens com.example.demo to javafx.fxml;
+    exports com.example.demo.Screen;
+    opens com.example.demo.Screen to javafx.fxml;
     exports com.example.demo.controller;
+    exports com.example.demo.Actor;
+    opens com.example.demo.Actor to javafx.fxml;
+    exports com.example.demo.Level;
+    opens com.example.demo.Level to javafx.fxml;
+    exports com.example.demo.Image;
+    opens com.example.demo.Image to javafx.fxml;
+    exports com.example.demo.Actor.User;
+    opens com.example.demo.Actor.User to javafx.fxml;
+    exports com.example.demo.Actor.Enemy;
+    opens com.example.demo.Actor.Enemy to javafx.fxml;
 }

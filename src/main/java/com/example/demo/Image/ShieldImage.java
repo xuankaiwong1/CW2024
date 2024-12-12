7fb0e92d838033ee2dade974302aa5b150af218f
xuankaiwong1 <wongxuankai123@gmail.com>
2024-12-12 23:22:27 +0800

    Make the shield appear change the shield image and let the shield spawned in boss level

--- src/main/java/com/example/demo/Image/ShieldImage.java
+++ src/main/java/com/example/demo/Image/ShieldImage.java
@@ -1,18 +1,18 @@
-package com.example.demo;
+package com.example.demo.Image;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class ShieldImage extends ImageView {
-	
-	private static final String IMAGE_NAME = "/images/shield.png";
-	private static final int SHIELD_SIZE = 200;
-	
+
+	private static final String IMAGE_NAME = "/com/example/demo/images/shield.png";
+	private static final int SHIELD_SIZE = 75;
+
 	public ShieldImage(double xPosition, double yPosition) {
 		this.setLayoutX(xPosition);
 		this.setLayoutY(yPosition);
-		//this.setImage(new Image(IMAGE_NAME));
-		this.setImage(new Image(getClass().getResource("/com/example/demo/images/shield.png").toExternalForm()));
+		//this.setImage(new com.example.demo.Image(IMAGE_NAME));
+		this.setImage(new Image(getClass().getResource(IMAGE_NAME).toExternalForm()));
 		this.setVisible(false);
 		this.setFitHeight(SHIELD_SIZE);
 		this.setFitWidth(SHIELD_SIZE);
@@ -20,10 +20,12 @@ public class ShieldImage extends ImageView {
 
 	public void showShield() {
 		this.setVisible(true);
+		System.out.println("The shield is now visible!");
 	}
-	
+
 	public void hideShield() {
 		this.setVisible(false);
+		System.out.println("The shield is now hidden!");
 	}
 
 }

d869909e1f0e0e5deee6c47e4d9b51a48eb8c73b
xuankaiwong1 <wongxuankai123@gmail.com>
2024-12-12 23:34:56 +0800

    Connect the main to main menu disable the full screen

--- src/main/java/com/example/demo/controller/Main.java
+++ src/main/java/com/example/demo/controller/Main.java
@@ -1,29 +1,20 @@
 package com.example.demo.controller;
 
-import java.lang.reflect.InvocationTargetException;
-
+import com.example.demo.Screen.MainMenu;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	private static final int SCREEN_WIDTH = 1300;
-	private static final int SCREEN_HEIGHT = 750;
-	private static final String TITLE = "Sky Battle";
-	private Controller myController;
-
 	@Override
-	public void start(Stage stage) throws ClassNotFoundException, NoSuchMethodException, SecurityException,
-			InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-		stage.setTitle(TITLE);
-		stage.setResizable(false);
-		stage.setHeight(SCREEN_HEIGHT);
-		stage.setWidth(SCREEN_WIDTH);
-		myController = new Controller(stage);
-		myController.launchGame();
+	public void start(Stage primaryStage) {
+		MainMenu mainMenu = new MainMenu();
+		mainMenu.start(primaryStage);
+
+		primaryStage.setResizable(false);
 	}
 
 	public static void main(String[] args) {
-		launch();
+		launch(args);
 	}
 }

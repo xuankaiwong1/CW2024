ffc5aaf88fe61b5302559cbbfe3fe98c4b9eee03
xuankaiwong1 <wongxuankai123@gmail.com>
2024-12-13 00:26:08 +0800

    Adjust the height and width of game over image.

--- src/main/java/com/example/demo/Image/GameOverImage.java
+++ src/main/java/com/example/demo/Image/GameOverImage.java
@@ -1,17 +1,19 @@
-package com.example.demo;
+package com.example.demo.Image;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class GameOverImage extends ImageView {
-	
+
 	private static final String IMAGE_NAME = "/com/example/demo/images/gameover.png";
+	private static final int HEIGHT = 600; // Adjust the height as needed
+	private static final int WIDTH = 700; // Adjust the width as needed
 
 	public GameOverImage(double xPosition, double yPosition) {
-		setImage(new Image(getClass().getResource(IMAGE_NAME).toExternalForm()) );
-//		setImage(ImageSetUp.getImageList().get(ImageSetUp.getGameOver()));
+		setImage(new Image(getClass().getResource(IMAGE_NAME).toExternalForm()));
+		setFitHeight(HEIGHT);
+		setFitWidth(WIDTH);
 		setLayoutX(xPosition);
 		setLayoutY(yPosition);
 	}
-
 }
